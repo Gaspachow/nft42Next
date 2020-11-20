a95f0627d9c69d573f7ac7a2985bf01b6c90a9a5
pacxiu <nedvedek111@gmail.com>
2020-11-20 13:13:34 +0100

    Add fetching user on localhost

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,23 +1,47 @@
 import { recoverPersonalSignature } from 'eth-sig-util';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { SIGNATURE_MESSAGE } from '../../constants';
+import { makeRequest } from '../../services/http';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { address, signature, code } = req.body;
 
   const signedAddress = recoverPersonalSignature({ data: SIGNATURE_MESSAGE, sig: signature });
 
   let verified = false;
 
-  console.info('addresses');
-  console.log(signedAddress.toLowerCase());
-  console.log(address.toLowerCase());
   if (signedAddress.toLowerCase() === (address as string).toLowerCase()) {
     verified = true;
   }
 
   // DO USER ID FETCHING FROM DISCORD
+  const data = {
+    client_id: process.env.CLIENT_ID as string,
+    client_secret: process.env.CLIENT_SECRET as string,
+    grant_type: 'authorization_code',
+    code: code as string,
+    redirect_uri:
+      process.env.NODE_ENV === 'production'
+        ? 'https://nft42-next.vercel.app/'
+        : 'http://localhost:3000',
+    scope: 'identify',
+  };
 
+  const accessToken = await makeRequest('https://discord.com/api/oauth2/token', {
+    body: new URLSearchParams(data),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  const user = await makeRequest('http://discordapp.com/api/users/@me', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + accessToken.access_token,
+    },
+  });
+
+  console.log(user);
   // DO SOME STUFF ON MONGODB
 
   return res.status(200).json({
