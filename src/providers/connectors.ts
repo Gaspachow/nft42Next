1248c1107c4e8d5941c145ca46dd8941142ffdd1
cesarsld <owlofmoistness@gmail.com>
2022-01-08 17:40:25 +0100

    Fix rpc url

--- src/providers/connectors.ts
+++ src/providers/connectors.ts
@@ -4,7 +4,7 @@ import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 
 const POLLING_INTERVAL = 12000
 const RPC_URLS: { [chainId: number]: string } = {
-  1: 'process.env.RPC_URL1' as string
+  1: process.env.RPC_URL1 as string
 }
 
 export const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42] })
